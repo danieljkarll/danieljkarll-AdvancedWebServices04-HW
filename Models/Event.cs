c3b4d309c166c53ccc3a04982d9cdff3b8dc8d9f
danielkarll <daieljkarll@gmail.com>
2020-02-10 19:37:00 -0600

    Lab 4 HW and lab
    Finished

---
+++ Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Modas.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool Flagged { get; set; }
+        // foreign key for location 
+        public int LocationId { get; set; }
+        // navigation property
+        public Location Location { get; set; }
+
+    }
+}
